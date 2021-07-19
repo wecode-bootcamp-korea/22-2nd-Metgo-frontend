3766b8e6fd8f46276e633a1c6ecb7b80f698b734
euiyeonlee <dmldus1121@naver.com>
2021-07-19 20:10:45 +0900

    초기세팅3

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Nav from '../src/components/Nav/Nav';
+import Main from '../src/pages/Main/Main';
+
 function Routes() {
   return (
     <Router>
