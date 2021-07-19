95a8b4c5d0727fd44e43574d0b746decf058b938
euiyeonlee <dmldus1121@naver.com>
2021-07-19 18:48:05 +0900

    dsstore

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,10 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}
-	
-	// 전역스타일
-	
-`;
+  ${reset}`;
 
 export default GlobalStyle;
